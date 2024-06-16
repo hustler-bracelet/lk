20876da7a55d221cb9a91bf022a06f32a02d2520
fJusk <piter.ruf@gmail.com>
2024-06-16 10:53:33 +0300

    fix refg

--- hustler_bracelet_lk/referral/service.py
+++ hustler_bracelet_lk/referral/service.py
@@ -17,13 +17,10 @@ class ReferralService:
 
     async def get_referred_users(self) -> list[User]:
         user_repository = get_user_repository(self._session)
-        all_referred_users = await user_repository.filter_by(
-            referred_by=await self._user.awaitable_attrs.telegram_id
+        all_referred_users = await user_repository.get_all_referals_with_subscription(
+            self._user.telegram_id,
         )
-        return [
-            user for user in all_referred_users
-            if await self._subscription_manager.get_user_subscription()
-        ]
+        return all_referred_users
 
     async def get_referral_link(self) -> str:
         telegram_id = await self._user.awaitable_attrs.telegram_id
--- hustler_bracelet_lk/repos/user.py
+++ hustler_bracelet_lk/repos/user.py
@@ -1,6 +1,32 @@
+import pytz
+
+from datetime import datetime
+from sqlalchemy import select
+
 from .generic import Repository
-from hustler_bracelet_lk.database.models import User
+from hustler_bracelet_lk.database.models import User, BraceletSubscription
 
 
-def get_user_repository(session) -> Repository[User]:
-    return Repository(User, session)
+def get_user_repository(session) -> 'UserRepository':
+    return UserRepository(session)
+
+
+class UserRepository(Repository[User]):
+    def __init__(self, session):
+        super().__init__(User, session)
+
+    async def get_all_referals_with_subscription(self, user_id: int) -> list[User]:
+        query = (
+            select(User)
+            .where(
+                User.referred_by == user_id,
+                User.telegram_id.in_(
+                    select(BraceletSubscription.telegram_id)
+                    .where(
+                        BraceletSubscription.will_end_on >= datetime.now(pytz.timezone('Europe/Moscow'))
+                    )
+                )
+            )
+        )
+
+        return (await self._session.execute(query)).scalars().all()
